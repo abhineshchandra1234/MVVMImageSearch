a7eaec940a3530df319540be12ef484cc8ceecb1
abhinesh <abhinesh.c@eegrab.com>
2021-04-06 15:50:56 +0530

    recycler page is opening properly,app is working fine

---
+++ app/src/main/java/com/eegrab/mvvmimagesearch/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.eegrab.mvvmimagesearch.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.eegrab.mvvmimagesearch.R
+
+class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+}
