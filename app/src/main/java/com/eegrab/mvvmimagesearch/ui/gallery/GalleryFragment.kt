fd686c28f90dee27f1902a49066804ea52c3d191
abhinesh <abhinesh.c@eegrab.com>
2021-04-07 15:53:28 +0530

    image is loading app is working fine

--- app/src/main/java/com/eegrab/mvvmimagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/eegrab/mvvmimagesearch/ui/gallery/GalleryFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.eegrab.mvvmimagesearch.R
+import com.eegrab.mvvmimagesearch.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -13,12 +14,29 @@ class GalleryFragment: Fragment(R.layout.fragment_gallery) {
 
     private val viewModel by viewModels<GalleryViewModel>()
 
+    private var binding: FragmentGalleryBinding? = null
+    private val _binding get() = binding!!
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
+        binding = FragmentGalleryBinding.bind(view)
 
+        val adapter = UnsplashPhotoAdapter()
+
+        _binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
         }
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
